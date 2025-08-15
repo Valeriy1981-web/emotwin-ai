8aa04ec993e1c4e44ac611e835a1eba60209c603
Valeriy1981-web <valerasv1981@rambler.ru>
2025-08-15 13:06:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))  # Render передаёт PORT через переменную окружения
     app.run(host='0.0.0.0', port=port)
